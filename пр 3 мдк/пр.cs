b0d8497bcf0f8b8c3882d75bbed087dc4d8cf02b
mariar12 <118889694+mariar12@users.noreply.github.com>
2023-05-04 14:24:45 +0300

    Update пр.cs

--- пр 3 мдк/пр.cs
+++ пр 3 мдк/пр.cs
@@ -25,3 +25,32 @@ Console.WriteLine("R = "+ Math.Round(R,4));
 }
 }
 }
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double a, x, b0, bk, delta_b, R, b;
+        Console.WriteLine("Введите a");
+        a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите x");
+        x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите b0");
+        b0 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите bk");
+        bk = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите delta_b");
+        delta_b = Convert.ToDouble(Console.ReadLine());
+        
+        b = b0;
+        while (b <= bk)
+        {
+            R = (2.4 * Math.Cos(Math.Pow(a, 3)) + Math.Sqrt(b * b - x)) / (Math.Exp(a) + b);
+            Console.WriteLine("R = " + Math.Round(R, 4));
+            b += delta_b;
+        }
+    }
+}
+
